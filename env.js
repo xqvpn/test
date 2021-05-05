13eb9e70ffc3ee82b1495005f33e5e51c0db3630
xqvpn <83118547+xqvpn@users.noreply.github.com>
2021-05-05 22:30:03 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,18 @@
+window.settings = {
+  // 站点标题
+  title: 'XQVPN-TEST',
+  // 站点描述
+  description: 'xqvpn is best',
+  // API
+  host: 'https://xqvpn.top',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: 'https://api.btstu.cn/sjbz/api.php?lx=dongman&format=images',
+  // crisp
+  crisp_id: ''
+}
